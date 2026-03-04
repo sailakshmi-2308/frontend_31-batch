d6c88e32964647920afd5157c44fb6220d2b0421
sai lakshmi <sailakshmi@codegnan.com>
2026-03-04 15:14:53 +0530

    march3

---
+++ javascript/external.js
@@ -0,0 +1,40 @@
+// "use strict"
+
+console.log("SANDEEEPPPPPP");
+
+// 25
+// let madhav //declaration
+// madhav=25  //assigining
+
+let madhav=25  //initilization
+
+// let madhav   // redeclare -NO
+madhav=78  //reassign- YES
+
+
+
+// let madhav=25;
+console.log(madhav)
+
+
+
+// let swathi="abcde"
+// const swathi; 
+const swathi="abcde"
+// console.log(swathi)
+
+// const swathi;  //redclare- NO
+// swathi="89"
+// console.log(swathi) //reassign- NO
+
+// var kwaja;
+// kwaja="thathayya"
+
+var kwaja="thathayya"
+
+
+var kwaja;
+kwaja="musalodu";
+console.log(kwaja)
+
+
